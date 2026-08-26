b8b0b90d26b4f94378e7b216d5ff3e35df561ef2
ZebastianEBR <sebastianballesterosruiz0@gmail.com>
2026-08-26 15:04:04 -0500

    ADD: se agrega borrador para bucle de seleccion de casilla por turnos

--- src/main/java/com/travelplanner/app/Main.java
+++ src/main/java/com/travelplanner/app/Main.java
@@ -1,12 +1,8 @@
 package com.travelplanner.app;
 
 import com.travelplanner.model.Jugador;
-import com.travelplanner.model.Juego;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -14,19 +10,83 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Map<Integer, String>> listaCasillas = new ArrayList<>();
-
-        listaCasillas = [{}];
         ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 
-        System.out.print("Ingresa tu nombre");
+
+        Map<Integer, String> c1 = new HashMap<>();
+        c1.put(1, "_");
+        listaCasillas.add(c1);
+
+        Map<Integer, String> c2 = new HashMap<>();
+        c2.put(2, "_");
+        listaCasillas.add(c2);
+
+        Map<Integer, String> c3 = new HashMap<>();
+        c3.put(3, "_");
+        listaCasillas.add(c3);
+
+        Map<Integer, String> c4 = new HashMap<>();
+        c4.put(4, "_");
+        listaCasillas.add(c4);
+
+        Map<Integer, String> c5 = new HashMap<>();
+        c5.put(5, "_");
+        listaCasillas.add(c5);
+
+        Map<Integer, String> c6 = new HashMap<>();
+        c6.put(6, "_");
+        listaCasillas.add(c6);
+
+        Map<Integer, String> c7 = new HashMap<>();
+        c7.put(7, "_");
+        listaCasillas.add(c7);
+
+        Map<Integer, String> c8 = new HashMap<>();
+        c8.put(8, "_");
+        listaCasillas.add(c8);
+
+        Map<Integer, String> c9 = new HashMap<>();
+        c9.put(9, "_");
+        listaCasillas.add(c9);
+
+
+//        c1.replace(1, "x");
+//        String valor = c1.get(1);
+//
+//        c2.replace(2, "0");
+//        String valor2 = c2.get(2);
+//
+//        c3.replace(3, "x");
+//        String valor3 = c3.get(3);
+//
+//        c4.replace(4, "x");
+//        String valor4 = c4.get(4);
+//
+//        c5.replace(5, "0");
+//        String valor5 = c5.get(5);
+//
+//        c6.replace(6, "0");
+//        String valor6 = c6.get(6);
+//
+//        c7.replace(7, "x");
+//        String valor7 = c7.get(7);
+//
+//        c8.replace(8, "0");
+//        String valor8 = c8.get(8);
+//
+//        c9.replace(9, "0");
+//        String valor9 = c9.get(9);
+
+
+        System.out.print("Ingresa tu nombre   ");
         String nombre1 = scanner.nextLine();
-        System.out.print("Bienvenido " + nombre1 + "!!!");
+        System.out.print("Bienvenido " + nombre1 + "!!!\n");
         String figura1 = "x";
 
-        System.out.print("Ingresa tu nombre");
+        System.out.print("Ingresa tu nombre   ");
         String nombre2 = scanner.nextLine();
-        System.out.print("Bienvenido " + nombre2 + "!!!");
-        String figura2 = "x";
+        System.out.print("Bienvenido " + nombre2 + "!!!\n");
+        String figura2 = "o";
 
         Jugador jugador1 = new Jugador(nombre1, figura1);
         Jugador jugador2 = new Jugador(nombre2, figura2);
@@ -34,8 +94,52 @@ public class Main {
         jugadores.add(jugador1);
         jugadores.add(jugador2);
 
-        Juego juego = new Juego();
-        juego.setJugadores
+        System.out.println("JUGADORES: \n" + jugadores);
+        System.out.println(listaCasillas);
+
+
+        String tabla = ("_" + c1.get(1) + "_|_" + c2.get(2)+ "_|_" + c3.get(3) + "_\n" +
+                "_" + c4.get(4)+ "_|_" + c5.get(5) + "_|_"+ c6.get(6) + "_\n" +
+                " " + c7.get(7)+ " | " + c8.get(8) + " | "+ c9.get(9) + "\n");
+
+        System.out.println(tabla);
+
+
+        // borrador de logica
+        boolean stop = true;
+        Jugador jugador = jugador1;
+
+        while (stop) {
+            System.out.println("va " + jugador);
+            System.out.print("En que casilla quieres jugar   ");
+            int posicion1 = Integer.parseInt(scanner.nextLine());
+
+
+            Map<Integer, String> casilla = listaCasillas.get(posicion1 - 1);
+            System.out.println(casilla);
+            System.out.println(casilla.get(posicion1));
+
+            if (casilla.get(posicion1) == "_") {
+                casilla.replace(posicion1, jugador.getFigura());
+                String tabla1 = ("_" + c1.get(1) + "_|_" + c2.get(2)+ "_|_" + c3.get(3) + "_\n" +
+                        "_" + c4.get(4)+ "_|_" + c5.get(5) + "_|_"+ c6.get(6) + "_\n" +
+                        " " + c7.get(7)+ " | " + c8.get(8) + " | "+ c9.get(9) + "\n");
+
+
+
+
+
+                if (jugador == jugador1) {
+                    jugador = jugador2;
+                } else if (jugador == jugador2) {
+                    jugador = jugador1;
+                }
+                System.out.println(tabla1);
+            } else {
+                System.out.println("esa casilla ya esta ocupada, sorry");
+                stop = false;
+            }
+        }
 
 
 
--- src/main/java/com/travelplanner/model/Jugador.java
+++ src/main/java/com/travelplanner/model/Jugador.java
@@ -5,28 +5,32 @@ public class Jugador {
     private String nombre;
     private String figura;
 
-    public Jugador(String nombre,String figura){
+    public Jugador(String nombre, String figura) {
 
         this.nombre = nombre;
         this.figura = figura;
     }
 
 
-
-    public String getFigura(){
+    public String getFigura() {
         return figura;
     }
 
-    public void setFigura(String figura){
+    public void setFigura(String figura) {
         this.figura = figura;
     }
 
 
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre){
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre = " + nombre + " y su figura es " + figura + "\n";
     }
 }
